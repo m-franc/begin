bf64d3a28e89bfcf1755dcfd376a2a421c469e21
mrchoung <maxime.franc1@gmail.com>
2024-03-18 19:02:35 +0100

    Django tuto, API database part, very interseseting

--- blog/apps.py
+++ blog/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls'
-
-    
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("J'ai commencé Django héhéhé")
+
+def detail(request, question_id):
+    response = "You're looking at question %s."
+    return HttpResponse(response % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s.")
+
+
